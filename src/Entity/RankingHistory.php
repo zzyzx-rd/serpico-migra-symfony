723fc1fd0070328ae5481e7019a76f8c5ce67d21
Guillaume de Granrut <guillaume.degranrut@gmail.com>
2020-08-11 16:38:24 +0200

    Ajout des fonctionnalités de RankingHistory

--- src/Entity/RankingHistory.php
+++ src/Entity/RankingHistory.php
@@ -12,7 +12,7 @@ use Doctrine\ORM\Mapping\ManyToOne;
  * @ApiResource()
  * @ORM\Entity(repositoryClass=RankingHistoryRepository::class)
  */
-class RankingHistory
+class RankingHistory extends DbObject
 {
     /**
      * @ORM\Id()
@@ -90,113 +90,161 @@ class RankingHistory
      */
     private $rhk_user_usr;
 
+    /**
+     * RankingHistory constructor.
+     * @param int $id
+     * @param $rkh_wtype
+     * @param $rkh_abs_result
+     * @param $rkh_rel_result
+     * @param $rkh_period
+     * @param $rkh_freq
+     * @param $rkh_value
+     * @param $rkh_series_pop
+     * @param $rkh_createdBy
+     * @param $rkh_inserted
+     * @param $activity
+     * @param $stage
+     * @param $criterion
+     * @param $rhk_user_usr
+     */
+    public function __construct(
+        int $id = 0,
+        $rkh_wtype = null,
+        $rkh_abs_result = null,
+        $rkh_rel_result = null,
+        $rkh_period = null,
+        $rkh_freq = null,
+        $rkh_value = null,
+        $rkh_series_pop = null,
+        $rkh_createdBy = null,
+        $rkh_inserted = null,
+        $activity = null,
+        $stage = null,
+        $criterion = null,
+        $rhk_user_usr = null)
+    {
+        $this->rkh_wtype = $rkh_wtype;
+        $this->rkh_abs_result = $rkh_abs_result;
+        $this->rkh_rel_result = $rkh_rel_result;
+        $this->rkh_period = $rkh_period;
+        $this->rkh_freq = $rkh_freq;
+        $this->rkh_value = $rkh_value;
+        $this->rkh_series_pop = $rkh_series_pop;
+        $this->rkh_inserted = $rkh_inserted;
+        $this->activity = $activity;
+        $this->stage = $stage;
+        $this->criterion = $criterion;
+        $this->rhk_user_usr = $rhk_user_usr;
+    }
+
+
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getRkhWtype(): ?string
+    public function getWtype(): ?string
     {
         return $this->rkh_wtype;
     }
 
-    public function setRkhWtype(string $rkh_wtype): self
+    public function setWtype(string $rkh_wtype): self
     {
         $this->rkh_wtype = $rkh_wtype;
 
         return $this;
     }
 
-    public function getRkhAbsResult(): ?int
+    public function getAbsResult(): ?int
     {
         return $this->rkh_abs_result;
     }
 
-    public function setRkhAbsResult(int $rkh_abs_result): self
+    public function setAbsResult(int $rkh_abs_result): self
     {
         $this->rkh_abs_result = $rkh_abs_result;
 
         return $this;
     }
 
-    public function getRkhRelResult(): ?float
+    public function getRelResult(): ?float
     {
         return $this->rkh_rel_result;
     }
 
-    public function setRkhRelResult(float $rkh_rel_result): self
+    public function setRelResult(float $rkh_rel_result): self
     {
         $this->rkh_rel_result = $rkh_rel_result;
 
         return $this;
     }
 
-    public function getRkhPeriod(): ?int
+    public function getPeriod(): ?int
     {
         return $this->rkh_period;
     }
 
-    public function setRkhPeriod(int $rkh_period): self
+    public function setPeriod(int $rkh_period): self
     {
         $this->rkh_period = $rkh_period;
 
         return $this;
     }
 
-    public function getRkhFreq(): ?int
+    public function getFreq(): ?int
     {
         return $this->rkh_freq;
     }
 
-    public function setRkhFreq(int $rkh_freq): self
+    public function setFreq(int $rkh_freq): self
     {
         $this->rkh_freq = $rkh_freq;
 
         return $this;
     }
 
-    public function getRkhValue(): ?float
+    public function getValue(): ?float
     {
         return $this->rkh_value;
     }
 
-    public function setRkhValue(float $rkh_value): self
+    public function setValue(float $rkh_value): self
     {
         $this->rkh_value = $rkh_value;
 
         return $this;
     }
 
-    public function getRkhSeriesPop(): ?int
+    public function getSeriesPop(): ?int
     {
         return $this->rkh_series_pop;
     }
 
-    public function setRkhSeriesPop(int $rkh_series_pop): self
+    public function setSeriesPop(int $rkh_series_pop): self
     {
         $this->rkh_series_pop = $rkh_series_pop;
 
         return $this;
     }
 
-    public function getRkhCreatedBy(): ?int
+    public function getCreatedBy(): ?int
     {
         return $this->rkh_createdBy;
     }
 
-    public function setRkhCreatedBy(?int $rkh_createdBy): self
+    public function setCreatedBy(?int $rkh_createdBy): self
     {
         $this->rkh_createdBy = $rkh_createdBy;
 
         return $this;
     }
 
-    public function getRkhInserted(): ?\DateTimeInterface
+    public function getInserted(): ?\DateTimeInterface
     {
         return $this->rkh_inserted;
     }
 
-    public function setRkhInserted(\DateTimeInterface $rkh_inserted): self
+    public function setInserted(\DateTimeInterface $rkh_inserted): self
     {
         $this->rkh_inserted = $rkh_inserted;
 
