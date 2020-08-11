76210ab5206c2378ffb2253878f4c76fe21b8816
Guillaume de Granrut <guillaume.degranrut@gmail.com>
2020-08-11 10:51:13 +0200

    Ajout des fonctionnalités de Country

--- src/Entity/Country.php
+++ src/Entity/Country.php
@@ -3,13 +3,15 @@
 namespace App\Entity;
 
 use App\Repository\CountryRepository;
+use DateTime;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\Mapping\OneToMany;
 
 /**
  * @ORM\Entity(repositoryClass=CountryRepository::class)
  */
-class Country
+class Country extends DbObject
 {
     /**
      * @ORM\Id()
@@ -53,65 +55,96 @@ class Country
      */
     private $firms;
 
+    /**
+     * Country constructor.
+     * @param $id
+     * @param $cou_abbr
+     * @param $cou_fullname
+     * @param $cou_name
+     * @param $cou_createdBy
+     * @param $cou_inserted
+     * @param $states
+     * @param $firms
+     */
+    public function __construct(
+        $id,
+        $cou_abbr = null,
+        $cou_fullname = null,
+        $cou_name = null,
+        $cou_createdBy= null,
+        $cou_inserted = null,
+        $states = null,
+        $firms = null)
+    {
+        parent::__construct($id, $cou_createdBy, new DateTime());
+        $this->cou_abbr = $cou_abbr;
+        $this->cou_fullname = $cou_fullname;
+        $this->cou_name = $cou_name;
+        $this->cou_inserted = $cou_inserted;
+        $this->states = $states?$states:new ArrayCollection();
+        $this->firms = $firms?$firms:new ArrayCollection();
+    }
+
+
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getCouAbbr(): ?string
+    public function getAbbr(): ?string
     {
         return $this->cou_abbr;
     }
 
-    public function setCouAbbr(string $cou_abbr): self
+    public function setAbbr(string $cou_abbr): self
     {
         $this->cou_abbr = $cou_abbr;
 
         return $this;
     }
 
-    public function getCouFullname(): ?string
+    public function getFullname(): ?string
     {
         return $this->cou_fullname;
     }
 
-    public function setCouFullname(string $cou_fullname): self
+    public function setFullname(string $cou_fullname): self
     {
         $this->cou_fullname = $cou_fullname;
 
         return $this;
     }
 
-    public function getCouName(): ?string
+    public function getName(): ?string
     {
         return $this->cou_name;
     }
 
-    public function setCouName(string $cou_name): self
+    public function setName(string $cou_name): self
     {
         $this->cou_name = $cou_name;
 
         return $this;
     }
 
-    public function getCouCreatedBy(): ?int
+    public function getCreatedBy(): ?int
     {
         return $this->cou_createdBy;
     }
 
-    public function setCouCreatedBy(?int $cou_createdBy): self
+    public function setCreatedBy(?int $cou_createdBy): self
     {
         $this->cou_createdBy = $cou_createdBy;
 
         return $this;
     }
 
-    public function getCouInserted(): ?\DateTimeInterface
+    public function getInserted(): ?\DateTimeInterface
     {
         return $this->cou_inserted;
     }
 
-    public function setCouInserted(\DateTimeInterface $cou_inserted): self
+    public function setInserted(\DateTimeInterface $cou_inserted): self
     {
         $this->cou_inserted = $cou_inserted;
 
@@ -148,6 +181,21 @@ class Country
     public function setFirms($firms): void
     {
         $this->firms = $firms;
+    }
+    function addFirm(WorkerFirm $firm){
+        $this->firms->add($firm);
+        $firm->setState($this);
+        return $this;
+    }
+
+    function removeFirm(WorkerFirm $firm){
+        $this->firms->removeElement($firm);
+        return $this;
+    }
+
+    public function __toString()
+    {
+        return (string) $this->id;
     }
 
 }
