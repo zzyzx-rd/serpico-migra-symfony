05bf4ec3f5b77c1d63e46fd2136c9051178d873e
Guillaume de Granrut <guillaume.degranrut@gmail.com>
2020-08-11 14:45:44 +0200

    Ajout des fonctionnalités de OptionName

--- src/Entity/OptionName.php
+++ src/Entity/OptionName.php
@@ -10,7 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ApiResource()
  * @ORM\Entity(repositoryClass=OptionNameRepository::class)
  */
-class OptionName
+class OptionName extends DbObject
 {
     /**
      * @ORM\Id()
@@ -43,6 +43,30 @@ class OptionName
      * @ORM\Column(type="datetime")
      */
     private $ona_inserted;
+
+    /**
+     * OptionName constructor.
+     * @param $id
+     * @param $ona_type
+     * @param $ona_name
+     * @param $ona_description
+     * @param $ona_createdBy
+     * @param $ona_inserted
+     */
+    public function __construct(
+        $id = 0,
+        $ona_type = null,
+        $ona_description = null,
+        $ona_name = null,
+        $ona_createdBy = null,
+        $ona_inserted = null)
+    {
+        $this->ona_type = $ona_type;
+        $this->ona_name = $ona_name;
+        $this->ona_description = $ona_description;
+        $this->ona_inserted = $ona_inserted;
+    }
+
 
     public function getId(): ?int
     {
@@ -107,5 +131,10 @@ class OptionName
         $this->ona_inserted = $ona_inserted;
 
         return $this;
+    }
+
+    public function __toString()
+    {
+        return (string) $this->id;
     }
 }
