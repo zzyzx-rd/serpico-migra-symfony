a396498f4fe344049d442b7395ef82ac6b303d97
Guillaume de Granrut <guillaume.degranrut@gmail.com>
2020-08-12 09:59:16 +0200

    Ajout des fonctionnalités de Team

--- src/Entity/Team.php
+++ src/Entity/Team.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\TeamRepository;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\Mapping\JoinColumn;
 use Doctrine\ORM\Mapping\ManyToOne;
@@ -15,7 +16,7 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
  * @ORM\Entity(repositoryClass=TeamRepository::class)
  * @UniqueEntity(fields={"name"},message="This name is already in use on that org.")
  */
-class Team
+class Team extends DbObject
 {
     /**
      * @ORM\Id()
@@ -79,77 +80,119 @@ class Team
      * @OneToMany(targetEntity="Target", mappedBy="team",cascade={"persist", "remove"}, orphanRemoval=true)
      */
     private $targets;
+
+    /**
+     * Team constructor.
+     * @param $id
+     * @param $tea_name
+     * @param $tea_weight_ini
+     * @param $tea_picture
+     * @param $tea_createdBy
+     * @param $tea_inserted
+     * @param $tea_deleted
+     * @param $organization
+     * @param $teamUsers
+     * @param $participations
+     * @param $grades
+     * @param $targets
+     */
+    public function __construct(
+        $id = 0,
+        $tea_name = null,
+        $tea_picture = null,
+        $tea_weight_ini = null,
+        $organization = null,
+        $tea_createdBy = null,
+        $tea_inserted = null,
+        $tea_deleted = null,
+        $teamUsers = null,
+        $participations = null,
+        $grades = null,
+        $targets = null)
+    {
+        $this->tea_name = $tea_name;
+        $this->tea_weight_ini = $tea_weight_ini;
+        $this->tea_picture = $tea_picture;
+        $this->tea_inserted = $tea_inserted;
+        $this->tea_deleted = $tea_deleted;
+        $this->organization = $organization;
+        $this->teamUsers = $teamUsers?$teamUsers:new ArrayCollection();
+        $this->participations = $participations?$participations: new ArrayCollection();
+        $this->grades = $grades?$grades:new ArrayCollection();
+        $this->targets = $targets?$targets: new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getTeaName(): ?string
+    public function getName(): ?string
     {
         return $this->tea_name;
     }
 
-    public function setTeaName(string $tea_name): self
+    public function setName(string $tea_name): self
     {
         $this->tea_name = $tea_name;
 
         return $this;
     }
 
-    public function getTeaWeightIni(): ?float
+    public function getWeightIni(): ?float
     {
         return $this->tea_weight_ini;
     }
 
-    public function setTeaWeightIni(float $tea_weight_ini): self
+    public function setWeightIni(float $tea_weight_ini): self
     {
         $this->tea_weight_ini = $tea_weight_ini;
 
         return $this;
     }
 
-    public function getTeaPicture(): ?string
+    public function getPicture(): ?string
     {
         return $this->tea_picture;
     }
 
-    public function setTeaPicture(?string $tea_picture): self
+    public function setPicture(?string $tea_picture): self
     {
         $this->tea_picture = $tea_picture;
 
         return $this;
     }
 
-    public function getTeaCreatedBy(): ?int
+    public function getCreatedBy(): ?int
     {
         return $this->tea_createdBy;
     }
 
-    public function setTeaCreatedBy(int $tea_createdBy): self
+    public function setCreatedBy(int $tea_createdBy): self
     {
         $this->tea_createdBy = $tea_createdBy;
 
         return $this;
     }
 
-    public function getTeaInserted(): ?\DateTimeInterface
+    public function getInserted(): ?\DateTimeInterface
     {
         return $this->tea_inserted;
     }
 
-    public function setTeaInserted(\DateTimeInterface $tea_inserted): self
+    public function setInserted(\DateTimeInterface $tea_inserted): self
     {
         $this->tea_inserted = $tea_inserted;
 
         return $this;
     }
 
-    public function getTeaDeleted(): ?\DateTimeInterface
+    public function getDeleted(): ?\DateTimeInterface
     {
         return $this->tea_deleted;
     }
 
-    public function setTeaDeleted(\DateTimeInterface $tea_deleted): self
+    public function setDeleted(\DateTimeInterface $tea_deleted): self
     {
         $this->tea_deleted = $tea_deleted;
 
@@ -175,7 +218,7 @@ class Team
     /**
      * @return mixed
      */
-    public function getTeamUsers()
+    public function getmUsers()
     {
         return $this->teamUsers;
     }
@@ -183,7 +226,7 @@ class Team
     /**
      * @param mixed $teamUsers
      */
-    public function setTeamUsers($teamUsers): void
+    public function setmUsers($teamUsers): void
     {
         $this->teamUsers = $teamUsers;
     }
@@ -235,5 +278,127 @@ class Team
     {
         $this->targets = $targets;
     }
+    function addTeamUser(TeamUser $teamUser){
 
+        $this->teamUsers->add($teamUser);
+        $teamUser->setTeam($this);
+        return $this;
+    }
+
+    function removeTeamUser(TeamUser $teamUser){
+
+        $this->teamUsers->removeElement($teamUser);
+        return $this;
+    }
+    public function getCurrentTeamUsers()
+    {
+        return $this->teamUsers->filter(function(TeamUser $tu){
+            return !$tu->isDeleted();
+        });
+    }
+
+    public function getCurrentTeamExtUsers()
+    {
+        return $this->getTeamExtUsers()->filter(function(TeamUser $tu){
+            return !$tu->isDeleted();
+        });
+    }
+
+    public function getPastTeamExtUsers()
+    {
+        return $this->getTeamExtUsers()->filter(function(TeamUser $tu){
+            return $tu->isDeleted();
+        });
+    }
+
+    public function getCurrentTeamIntUsers()
+    {
+        return $this->getTeamIntUsers()->filter(function(TeamUser $tu){
+            return !$tu->isDeleted();
+        });
+    }
+
+    public function getPastTeamIntUsers()
+    {
+        return $this->getTeamIntUsers()->filter(function(TeamUser $tu){
+            return $tu->isDeleted();
+        });
+    }
+
+    function addTeamIntUser(TeamUser $teamUser)
+    {
+        $this->teamUsers->add($teamUser);
+        $teamUser->setTeam($this);
+        return $this;
+    }
+
+    function removeTeamIntUser(TeamUser $teamUser)
+    {
+        $this->teamUsers->removeElement($teamUser);
+        return $this;
+    }
+
+    function addTeamExtUser(TeamUser $teamUser)
+    {
+        $this->teamUsers->add($teamUser);
+        $teamUser->setTeam($this);
+        return $this;
+    }
+
+    function removeTeamExtUser(TeamUser $teamUser)
+    {
+        $this->teamUsers->removeElement($teamUser);
+        return $this;
+    }
+    public function getActiveTeamUsers()
+    {
+        $activeTeamUsers = new ArrayCollection;
+        foreach ($this->teamUsers as $teamUser) {
+            if ($teamUser->isDeleted() == false){
+                $activeTeamUsers->add($teamUser);
+            }
+        }
+        return $activeTeamUsers;
+    }
+    function addGrade(Grade $grade){
+
+        $this->grades->add($grade);
+        $grade->setParticipant($this);
+        return $this;
+    }
+
+    function removeGrade(Grade $grade){
+        $this->grades->removeElement($grade);
+        return $this;
+    }
+
+    public function toArray()
+    {
+        return [
+            'id' => $this->id,
+            'picture'=> $this->picture,
+            'name' => $this->name,
+        ];
+    }
+    function addTarget(Target $target){
+        $this->targets->add($target);
+        $target->setTeam($this);
+        return $this;
+    }
+
+    function removeTarget(Target $target){
+        $this->targets->removeElement($target);
+        return $this;
+    }
+    public function isOverviewable()
+    {
+        return $this->isModifiable();
+    }
+
+    public function __toString()
+    {
+        return (string)$this->id;
+    }
+
+    //TODO getIntTeamUser et le get Average et les trucs qui suivent
 }
