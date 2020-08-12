40b7eee54f01f8f6e0c3c7d2b8f97131b77e6e88
Guillaume de Granrut <guillaume.degranrut@gmail.com>
2020-08-12 10:57:01 +0200

    Ajout des fonctionnalités de WorkerExperience

--- src/Entity/WorkerExperience.php
+++ src/Entity/WorkerExperience.php
@@ -12,7 +12,7 @@ use Doctrine\ORM\Mapping\ManyToOne;
  * @ApiResource()
  * @ORM\Entity(repositoryClass=WorkerExperienceRepository::class)
  */
-class WorkerExperience
+class WorkerExperience extends DbObject
 {
     /**
      * @ORM\Id()
@@ -68,89 +68,125 @@ class WorkerExperience
      * @JoinColumn(name="worker_firm_wfi", referencedColumnName="wfi_id",nullable=false)
      */
     protected $firm;
+
+    /**
+     * WorkerExperience constructor.
+     * @param int $id
+     * @param $wex_active
+     * @param $wex_position
+     * @param $wex_location
+     * @param $wex_startdate
+     * @param $wex_enddate
+     * @param $wex_createdBy
+     * @param $wex_inserted
+     * @param $individual
+     * @param $firm
+     */
+    public function __construct(
+        int $id = 0,
+        $wex_active = null,
+        $wex_position = null,
+        $wex_location = null,
+        $wex_startdate = null,
+        $wex_enddate = null,
+        $wex_createdBy = null,
+        $wex_inserted = null,
+        $individual = null,
+        $firm = null)
+    {
+        $this->wex_active = $wex_active;
+        $this->wex_position = $wex_position;
+        $this->wex_location = $wex_location;
+        $this->wex_startdate = $wex_startdate;
+        $this->wex_enddate = $wex_enddate;
+        $this->wex_inserted = $wex_inserted;
+        $this->individual = $individual;
+        $this->firm = $firm;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getWexActive(): ?bool
+    public function getActive(): ?bool
     {
         return $this->wex_active;
     }
 
-    public function setWexActive(bool $wex_active): self
+    public function setActive(bool $wex_active): self
     {
         $this->wex_active = $wex_active;
 
         return $this;
     }
 
-    public function getWexPosition(): ?string
+    public function getPosition(): ?string
     {
         return $this->wex_position;
     }
 
-    public function setWexPosition(string $wex_position): self
+    public function setPosition(string $wex_position): self
     {
         $this->wex_position = $wex_position;
 
         return $this;
     }
 
-    public function getWexLocation(): ?string
+    public function getLocation(): ?string
     {
         return $this->wex_location;
     }
 
-    public function setWexLocation(string $wex_location): self
+    public function setLocation(string $wex_location): self
     {
         $this->wex_location = $wex_location;
 
         return $this;
     }
 
-    public function getWexStartdate(): ?\DateTimeInterface
+    public function getStartdate(): ?\DateTimeInterface
     {
         return $this->wex_startdate;
     }
 
-    public function setWexStartdate(\DateTimeInterface $wex_startdate): self
+    public function setStartdate(\DateTimeInterface $wex_startdate): self
     {
         $this->wex_startdate = $wex_startdate;
 
         return $this;
     }
 
-    public function getWexEnddate(): ?\DateTimeInterface
+    public function getEnddate(): ?\DateTimeInterface
     {
         return $this->wex_enddate;
     }
 
-    public function setWexEnddate(\DateTimeInterface $wex_enddate): self
+    public function setEnddate(\DateTimeInterface $wex_enddate): self
     {
         $this->wex_enddate = $wex_enddate;
 
         return $this;
     }
 
-    public function getWexCreatedBy(): ?int
+    public function getCreatedBy(): ?int
     {
         return $this->wex_createdBy;
     }
 
-    public function setWexCreatedBy(int $wex_createdBy): self
+    public function setCreatedBy(int $wex_createdBy): self
     {
         $this->wex_createdBy = $wex_createdBy;
 
         return $this;
     }
 
-    public function getWexInserted(): ?\DateTimeInterface
+    public function getInserted(): ?\DateTimeInterface
     {
         return $this->wex_inserted;
     }
 
-    public function setWexInserted(\DateTimeInterface $wex_inserted): self
+    public function setInserted(\DateTimeInterface $wex_inserted): self
     {
         $this->wex_inserted = $wex_inserted;
 
