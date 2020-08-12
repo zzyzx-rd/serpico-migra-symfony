6bbe9c6dfd0cbe522e429e793f56e1051bf8866a
Guillaume de Granrut <guillaume.degranrut@gmail.com>
2020-08-12 10:12:10 +0200

    Ajout des fonctionnalités de TemplateActivity

--- src/Entity/TemplateActivity.php
+++ src/Entity/TemplateActivity.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\TemplateActivityRepository;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\Mapping\JoinColumn;
@@ -15,7 +16,7 @@ use Doctrine\ORM\Mapping\OrderBy;
  * @ApiResource()
  * @ORM\Entity(repositoryClass=TemplateActivityRepository::class)
  */
-class TemplateActivity
+class TemplateActivity extends DbObject
 {
     /**
      * @ORM\Id()
@@ -24,7 +25,12 @@ class TemplateActivity
      * @var int
      */
     private $id;
-
+    /**
+     * @Column(name="act_magnitude", type="integer")
+     * @var int
+     */
+    protected $magnitude;
+    
     /**
      * @ORM\Column(type="boolean")
      */
@@ -95,90 +101,149 @@ class TemplateActivity
      * @ORM\ManyToOne(targetEntity=User::class)
      */
     private $act_master_usr;
+    /**
+     * @var DateTime
+     */
+    private $startdate;
+    /**
+     * @var DateTime
+     */
+    private $enddate;
+
+    /**
+     * TemplateActivity constructor.
+     * @param int $id
+     * @param int $magnitude
+     * @param $act_simplified
+     * @param $act_name
+     * @param $act_visibility
+     * @param $act_objectives
+     * @param $act_createdBy
+     * @param $act_inserted
+     * @param $act_saved
+     * @param $organization
+     * @param $department
+     * @param $recurring
+     * @param TemplateStage[]|Collection $stages
+     * @param $participants
+     * @param $act_master_usr
+     */
+    public function __construct(
+        int $id,
+        $act_master_usr,
+        $magnitude = 1,
+        $act_simplified = true,
+        $act_name = null,
+        $act_visibility = 'public',
+        $act_objectives = '',
+        $act_createdBy = null,
+        $act_inserted = null,
+        $act_saved = null,
+        $organization = null,
+        $department = null,
+        $recurring = null,
+        $stages = null,
+        $participants = null)
+    {
+        $this->magnitude = $magnitude;
+        $this->act_simplified = $act_simplified;
+        $this->act_name = $act_name;
+        $this->act_visibility = $act_visibility;
+        $this->act_objectives = $act_objectives;
+        $this->act_inserted = $act_inserted;
+        $this->act_saved = $act_saved;
+        $this->organization = $organization;
+        $this->department = $department;
+        $this->recurring = $recurring;
+        $this->stages = $stages?$stages: new ArrayCollection();
+        $this->participants = $participants?$participants:new ArrayCollection();
+        $this->act_master_usr = $act_master_usr;
+    }
+
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getActSimplified(): ?bool
+    public function getSimplified(): ?bool
     {
         return $this->act_simplified;
     }
 
-    public function setActSimplified(bool $act_simplified): self
+    public function setSimplified(bool $act_simplified): self
     {
         $this->act_simplified = $act_simplified;
 
         return $this;
     }
 
-    public function getActName(): ?string
+    public function getName(): ?string
     {
         return $this->act_name;
     }
 
-    public function setActName(string $act_name): self
+    public function setName(string $act_name): self
     {
         $this->act_name = $act_name;
 
         return $this;
     }
 
-    public function getActVisibility(): ?string
+    public function getVisibility(): ?string
     {
         return $this->act_visibility;
     }
 
-    public function setActVisibility(string $act_visibility): self
+    public function setVisibility(string $act_visibility): self
     {
         $this->act_visibility = $act_visibility;
 
         return $this;
     }
 
-    public function getActObjectives(): ?string
+    public function getObjectives(): ?string
     {
         return $this->act_objectives;
     }
 
-    public function setActObjectives(string $act_objectives): self
+    public function setObjectives(string $act_objectives): self
     {
         $this->act_objectives = $act_objectives;
 
         return $this;
     }
 
-    public function getActCreatedBy(): ?int
+    public function getCreatedBy(): ?int
     {
         return $this->act_createdBy;
     }
 
-    public function setActCreatedBy(int $act_createdBy): self
+    public function setCreatedBy(int $act_createdBy): self
     {
         $this->act_createdBy = $act_createdBy;
 
         return $this;
     }
 
-    public function getActInserted(): ?\DateTimeInterface
+    public function getInserted(): ?\DateTimeInterface
     {
         return $this->act_inserted;
     }
 
-    public function setActInserted(\DateTimeInterface $act_inserted): self
+    public function setInserted(\DateTimeInterface $act_inserted): self
     {
         $this->act_inserted = $act_inserted;
 
         return $this;
     }
 
-    public function getActSaved(): ?\DateTimeInterface
+    public function getSaved(): ?\DateTimeInterface
     {
         return $this->act_saved;
     }
 
-    public function setActSaved(\DateTimeInterface $act_saved): self
+    public function setSaved(\DateTimeInterface $act_saved): self
     {
         $this->act_saved = $act_saved;
 
@@ -265,16 +330,155 @@ class TemplateActivity
         $this->participants = $participants;
     }
 
-    public function getActMasterUsr(): ?User
+    public function getMasterUsr(): ?User
     {
         return $this->act_master_usr;
     }
 
-    public function setActMasterUsr(?User $act_master_usr): self
+    public function setMasterUsr(?User $act_master_usr): self
     {
         $this->act_master_usr = $act_master_usr;
 
         return $this;
     }
+    function addParticipant(TemplateActivityUser $participant)
+    {
+        $this->participants->add($participant);
+        $participant->setivity($this);
+        return $this;
+    }
+
+    function removeParticipant(TemplateActivityUser $participant)
+    {
+        $this->participants->removeElement($participant);
+        return $this;
+    }
+
+    function addStage(TemplateStage $stage)
+    {
+
+        $this->stages->add($stage);
+        $stage->setivity($this);
+        return $this;
+    }
+
+    function removeStage(TemplateStage $stage)
+    {
+        $this->stages->removeElement($stage);
+        return $this;
+    }
+
+    function addActiveStage(TemplateStage $stage)
+    {
+
+        $this->stages->add($stage);
+        $stage->setivity($this);
+        return $this;
+    }
+
+    function removeActiveStage(TemplateStage $stage)
+    {
+        $this->stages->removeElement($stage);
+        return $this;
+    }
+    public function getLatestStage()
+    {
+        $returnedStage = $this->stages->first();
+        foreach ($this->stages as $stage) {
+            if ($stage->getEnddate() > $returnedStage) {
+                $returnedStage = $stage;
+            }
+        }
+        return $returnedStage;
+    }
+
+    public function __toString()
+    {
+        return (string) $this->id;
+    }
+
+    public function hasParticipants()
+    {
+        foreach ($this->getActiveStages() as $stage){
+            if (count($stage->getParticipants()) > 1){
+                return true;
+            }
+        }
+        return false;
+    }
+    /**
+     * @return DateTime
+     */
+    public function getStartdate()
+    {
+        $startDate = new DateTime('2099-01-01');
+        foreach ($this->stages as $stage) {
+            if ($stage->getStartdate() < $startDate) {
+                $startDate = $stage->getStartDate();
+            }
+        }
+        return $startDate;
+    }
+
+    public function getEnddate()
+    {
+        $enddate = new DateTime('2000-01-01');
+        foreach ($this->getStages() as $stage) {
+            if ($stage->getEnddate() > $enddate) {
+                $enddate = $stage->getEnddate();
+            }
+        }
+        return $enddate;
+    }
+
+    /**
+     * @param DateTime $startdate
+     * @return TemplateActivity
+     */
+    public function setStartdate($startdate)
+    {
+        $this->startdate = $startdate;
+        return $this;
+    }
+
+
+    /**
+     * @param DateTime $enddate
+     * @return TemplateActivity
+     */
+    public function setEnddate($enddate)
+    {
+        $this->enddate = $enddate;
+        return $this;
+    }
+    function addActiveModifiableStage(TemplateStage $stage)
+    {
+        if (!$stage->getActivity()) {
+            $this->stages->add($stage);
+            $stage->setActivity($this);
+            return $this;
+        }
+    }
+
+    function removeActiveModifiableStage(TemplateStage $stage)
+    {
+        $this->stages->removeElement($stage);
+        //$stage->setActivity(null);
+        return $this;
+    }
+
+    /**
+     * An activity template is *never* finalized by definition,
+     * however this getter is necessary in various templates
+     */
+    function isFinalized() {
+        return false;
+    }
+
+    public function userCanEdit(User $u)
+    {
+        return $u->getRole() == 1 || $u->getRole() == 4 || $this->masterUserId == $u->getId();
+    }
+    
 
 }
