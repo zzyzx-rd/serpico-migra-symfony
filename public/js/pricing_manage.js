bcba01269efb52bac189fc4bb149ac9cc40bfaef
Guillaume Chatelain <guitchat@gmail.com>
2020-12-03 12:28:30 +0100

    pricing page revamped (finished)

--- public/js/pricing_manage.js
+++ public/js/pricing_manage.js
@@ -4,21 +4,34 @@ $(document).ready(function () {
 
     $(this).find('.user-slider')[0].noUiSlider.on('slide', function (values, handle) {
 
-        $('.user-slider')[0].nextElementSibling.innerHTML = Number(values[handle]);
+        nbUsers =  Number(values[handle]);
+        yearlyPrice = $('.switch input').is(':checked');
+        divider = yearlyPrice ? 10 : 1;
+        if($(window).width() < 600){
+            if(nbUsers >= 14 / divider && nbUsers < 141 / divider){
+                $('.price-value').css('font-size','35px');
+            } else if (nbUsers >= 140 / divider && nbUsers < 1401 / divider) {
+                $('.price-value').css('font-size','25px');
+            } else if(nbUsers >= 1400 / divider){
+                $('.price-value').css('font-size','20px');
+            }
+        }
+        
+        $('.user-slider')[0].nextElementSibling.innerHTML = nbUsers;
 
         var price_1 = $('.switch input').is(':checked') ? 70 : 7;
         var price_2 = $('.switch input').is(':checked') ? 60 : 6;
         var price_3 = $('.switch input').is(':checked') ? 50 : 5;
         $('.price-value')[0].innerHTML =
-            Math.min(100,Number(values[handle])) * price_1 + Math.max(0, Math.min(150, Number(values[handle]) - 100)) * price_2 + Math.max(0,Math.min(1000,Number(values[handle]) - 250)) * price_3;
-        console.log(Math.min(100,Number(values[handle])) * price_1 + Math.max(0, Math.min(150, Number(values[handle]) - 100)) * price_2 + Math.max(0,Math.min(1000,Number(values[handle]) - 250)) * price_3);
+            Math.min(100,nbUsers) * price_1 + Math.max(0, Math.min(150, nbUsers - 100)) * price_2 + Math.max(0,Math.min(1000,nbUsers - 250)) * price_3;
+        console.log(Math.min(100,nbUsers) * price_1 + Math.max(0, Math.min(150, nbUsers - 100)) * price_2 + Math.max(0,Math.min(1000,nbUsers - 250)) * price_3);
 
        $userElmt = $('.user-nb');
 
-        if(Number(values[handle]) == 1){
+        if(nbUsers == 1){
             $userElmt.text($userElmt.text().slice(0,-1));
         }
-        if(Number(values[handle]) > 1 && $userElmt.text().charAt($userElmt.text().length - 1) != 's'){
+        if(nbUsers > 1 && $userElmt.text().charAt($userElmt.text().length - 1) != 's'){
             $userElmt.text($userElmt.text() + 's');
         }
     });
