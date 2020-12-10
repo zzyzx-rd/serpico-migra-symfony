0715ffb3e4541b632f5cbf33245c9e269fb50c03
Guillaume Chatelain <guitchat@gmail.com>
2020-12-10 14:16:52 +0100

    404 errors unlistened in prod

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -34,76 +34,68 @@ class ExceptionListener {
     {
         $request = $event->getRequest();
 
-        // We only store errors happening in prod environment, others are manager through SF error interface/profiler
-        if($request->server->get('APP_ENV') !='dev'){
+        // We only store non 404 errors happening in prod environment, others are manager through SF error interface/profiler
+        if($request->server->get('APP_ENV') !='dev' && $event->getResponse()->getStatusCode() != 404 && $event->getThrowable()){
 
             $mailer = $this->mailer;
             $container = $this->container;
             $twig = $this->twig;
+            $em = $this->em;
+            if (!$em->isOpen()) {
+                $em = $container->get('doctrine')->resetManager();  
+            }
             /** @var User|null */
             $user = $this->security->getUser();
             $usrId = $user ? $user->getId() : null;
-            
-            if($event->getThrowable()){
+            $throwable = $event->getThrowable();
+            $headers = $request->headers;
+            $attributes = $request->attributes;
+            $data = [];
+            $data['company_name'] = 'Dealdrive';
+            $data['logo_width_px'] = 110;
+            $data['address'] = '38, route d\'Esch';
+            $data['zipcode_city'] = 'L-1470 Luxembourg';
+            $data['phone'] = '+352 28 79 97 18';
+            $data['requestURI'] = $request->getRequestUri();
+            $data['method'] = $request->getMethod();
+            $data['route'] = $attributes->get('_route');
+            $data['user_agent'] = $headers->get('user_agent');
+            $data['referer'] = $headers->get('referer') ? basename($headers->get('referer')) : null;
+            $data['locale'] = $request->getLocale();
+            $data['file'] = basename($throwable->getFile());
+            $data['line'] = $throwable->getLine();
+            $data['message'] = $throwable->getMessage();
+            $data['user'] = $user;
+
+            $error = $em->getRepository(GeneratedError::class)->findOneBy(['message' => $data['message'], 'file' => $data['file']]);
+            if(!$error){
+
+                    $error = new GeneratedError();
+                    $error->setUsrId($usrId)
+                    ->setMethod($data['method'])
+                    ->setRequestURI($data['requestURI'])
+                    ->setRoute($data['route'])
+                    ->setAgent($data['user_agent'])
+                    ->setReferer($data['referer'])
+                    ->setLocale($data['locale'])
+                    ->setFile($data['file'])
+                    ->setLine($data['line'])
+                    ->setMessage($data['message']);
                 
-                $throwable = $event->getThrowable();
-                $file = basename($throwable->getFile());
+                $em->persist($error);
+                $em->flush();
                 
-                if($file != "RouterListener.php"){
-                    
-                    $em = $this->em;
-                    if (!$em->isOpen()) {
-                        $em = $container->get('doctrine')->resetManager();  
-                    }
-                    $headers = $request->headers;
-                    $attributes = $request->attributes;
-                    $data = [];
-                    $data['company_name'] = 'Dealdrive';
-                    $data['logo_width_px'] = 110;
-                    $data['address'] = '38, route d\'Esch';
-                    $data['zipcode_city'] = 'L-1470 Luxembourg';
-                    $data['phone'] = '+352 28 79 97 18';
-                    $data['requestURI'] = $request->getRequestUri();
-                    $data['method'] = $request->getMethod();
-                    $data['route'] = $attributes->get('_route');
-                    $data['user_agent'] = $headers->get('user_agent');
-                    $data['referer'] = $headers->get('referer') ? basename($headers->get('referer')) : null;
-                    $data['locale'] = $request->getLocale();
-                    $data['file'] = $file;
-                    $data['line'] = $throwable->getLine();
-                    $data['message'] = $throwable->getMessage();
-                    $data['user'] = $user;
-    
-                    $error = $em->getRepository(GeneratedError::class)->findOneBy(['message' => $data['message'], 'file' => $data['file']]);
-                    if(!$error){
-    
-                            $error = new GeneratedError();
-                            $error->setUsrId($usrId)
-                            ->setMethod($data['method'])
-                            ->setRequestURI($data['requestURI'])
-                            ->setRoute($data['route'])
-                            ->setAgent($data['user_agent'])
-                            ->setReferer($data['referer'])
-                            ->setLocale($data['locale'])
-                            ->setFile($data['file'])
-                            ->setLine($data['line'])
-                            ->setMessage($data['message']);
-                        
-                        $em->persist($error);
-                        $em->flush();
-                        
-                        $email = new TemplatedEmail();
-                        $mailTemplate = $twig->load('mails/errorNotification.html.twig');
-                        $email->from('Dealdrive <no-reply@dealdrive.app>')
-                        ->to('support@dealdrive.lu')
-                        ->subject($mailTemplate->renderBlock('subject', $data))
-                        ->htmlTemplate('mails/errorNotification.html.twig')
-                        ->context($data)
-                        ->embedFromPath('lib/img/logo_dd_p_l.png','logo_img');
-                        $mailer->send($email);
-                    }
-                }
+                $email = new TemplatedEmail();
+                $mailTemplate = $twig->load('mails/errorNotification.html.twig');
+                $email->from('Dealdrive <no-reply@dealdrive.app>')
+                ->to('support@dealdrive.lu')
+                ->subject($mailTemplate->renderBlock('subject', $data))
+                ->htmlTemplate('mails/errorNotification.html.twig')
+                ->context($data)
+                ->embedFromPath('lib/img/logo_dd_p_l.png','logo_img');
+                $mailer->send($email);
             }
+            
         }
 
         return;
